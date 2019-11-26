970cd8c64152ecd652a0f21e553001d17545b82e
Yunhao Tian <18373444@buaa.edu.cn>
2019-11-26 22:35:58 +0800

    Extend IM to 4096 words.

--- im.v
+++ im.v
@@ -5,20 +5,19 @@ module InstructionMemory(
            input absJump,
            input pcStall,
            input [31:0] absJumpAddress, // In bytes
-           output [31:0] pc,
+           output reg [31:0] pc,
            output [31:0] instruction
        );
 
 
-reg [31:0] memory [1023:0];
+reg [31:0] memory [4095:0];
 
 initial begin
     $readmemh("code.txt", memory);
 end
 
-reg [9:0] internalCounter;
-assign pc = {20'b11, internalCounter, 2'b0};
-assign instruction = memory[internalCounter];
+wire [31:0] realAddress = pc - 32'h3000;
+assign instruction = memory[realAddress[13:2]];
 
 always @(posedge clk) begin
 `ifdef DEBUG
@@ -26,7 +25,7 @@ always @(posedge clk) begin
 `endif
     if (reset)
     begin
-        internalCounter <= 0;
+        pc <= 32'h3000;
     end
     else if (pcStall) begin
 `ifdef VERBOSE
@@ -36,19 +35,19 @@ always @(posedge clk) begin
     end
     else if (absJump) begin
 `ifdef VERBOSE
-        $display("Jump to %h", { absJumpAddress[11:2], 2'b00 });
+        $display("Jump to %h", absJumpAddress);
 `endif
 
-        internalCounter <= absJumpAddress[11:2];
+        pc <= absJumpAddress;
     end
     else begin
-        internalCounter <= internalCounter + 1;
+        pc <= pc + 4;
     end
 end
 
 always @(*) begin
 `ifdef VERBOSE
-    $display("PC @ %h", internalCounter << 2);
+    $display("PC @ %h", pc);
 `endif
 end
 
