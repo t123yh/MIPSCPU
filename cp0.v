4e6c253cdf03065cbf1ec9b76e47713126f4878e
Yunhao Tian <18373444@buaa.edu.cn>
2019-12-10 17:50:32 +0800

    Add initialization for other bits of CP0.

--- cp0.v
+++ cp0.v
@@ -53,6 +53,12 @@ always @(*) begin
         end
     end
 end
+integer i;
+initial begin
+    for (i = 0; i < 16; i = i + 1) begin
+        registers[i] = 32'b0;
+    end
+end
 
 always @(posedge clk) begin
     if (reset) begin
