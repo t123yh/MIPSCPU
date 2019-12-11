f4e360e13f1e6c5b4a40986598065526ac5954e5
Yunhao Tian <18373444@buaa.edu.cn>
2019-12-11 11:13:24 +0800

    Separate memory and ram.

---
+++ ram.v
@@ -0,0 +1,33 @@
+
+module RAM(
+           input clk,
+           input reset,
+           input writeEnable,
+           input readEnable,
+           input [13:0] address,
+           input [31:0] writeDataIn,
+           output reg [31:0] readData,
+           output exception
+);
+reg [31:0] memory [4095:0];
+wire [11:0] realAddress = address[13:2];
+always @(*) begin
+    readData = 'bx;
+    if (readEnable) begin
+        readData = memory[realAddress];
+    end
+end
+
+integer i;
+always @(posedge clk) begin
+    if (reset) begin
+        for (i = 0; i < 4096; i = i + 1) begin
+            memory[i] <= 0;
+        end
+    end
+    else if (writeEnable) begin
+        memory[realAddress] <= writeDataIn;
+    end
+end
+
+endmodule
