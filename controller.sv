abda703b7defb5decc028452b5b3521357151d9a
Yunhao Tian <18373444@buaa.edu.cn>
2020-02-22 15:32:27 +0800

    Convert controller to SystemVerilog.

--- controller.sv
+++ controller.sv
@@ -7,33 +7,34 @@ module Controller (
            input reset,
            input bubble,
 
-           output reg [4:0] regRead1,
-           output reg [4:0] regRead2,
-           output reg regRead1Required,
-           output reg regRead2Required,
+           output logic [4:0] regRead1,
+           output logic [4:0] regRead2,
+           output logic regRead1Required,
+           output logic regRead2Required,
 
-           output reg [3:0] mulCtrl,
+           output logic [3:0] mulCtrl,
            output mulEnable,
-           output reg mulOutputSel,
+           output logic mulOutputSel,
 
-           output reg memLoad,
-           output reg [1:0] memWidthCtrl,
-           output reg memReadSignExtend,
-           output reg memStore,
-           output reg branch,
-           output reg [31:0] immediate,
-           output reg [4:0] destinationRegister,
-           output reg aluSrc,
-           output reg [3:0] aluCtrl,
-           output reg [3:0] cmpCtrl,
-           output reg absJump,
-           output reg absJumpLoc, // 1 = immediate, 0 = register
-           output reg [3:0] grfWriteSource,
-           output reg checkOverflow,
-           output reg [2:0] generateException,
-           output reg writeCP0,
-           output reg [4:0] numberCP0,
-           output reg bye
+           output logic memLoad,
+           output logic [1:0] memWidthCtrl,
+           output logic memReadSignExtend,
+           output logic memStore,
+           output logic branch,
+           output logic [31:0] immediate,
+           output logic [4:0] destinationRegister,
+           output logic aluSrc,
+           output logic [3:0] aluCtrl,
+           output logic [3:0] cmpCtrl,
+           output logic absJump,
+           output logic absJumpLoc, // 1 = immediate, 0 = register
+           output logic [3:0] grfWriteSource,
+           output logic checkOverflow,
+           output logic [2:0] generateException,
+           output logic writeCP0,
+           output logic [4:0] numberCP0,
+
+           output logic bye
        );
 
 wire [5:0] opcode = instruction[31:26];
@@ -52,6 +53,7 @@ wire [31:0] signExtendedImmediate = $signed(imm);
 assign mulEnable = mulCtrl != `mtDisabled;
 
 localparam R = 6'b000000;
+localparam REGIMM = 6'b000001;
 localparam ori = 6'b001101;
 localparam andi = 6'b001100;
 localparam xori = 6'b001110;
@@ -61,7 +63,6 @@ localparam beq = 6'b000100;
 localparam bne = 6'b000101;
 localparam blez = 6'b000110;
 localparam bgtz = 6'b000111;
-localparam bltz_bgez = 6'b000001;
 localparam lui = 6'b001111;
 localparam jal = 6'b000011;
 localparam addiu = 6'b001001;
@@ -104,6 +105,10 @@ localparam madd = 6'b000000;
 localparam maddu = 6'b000001;
 localparam eret = 6'b011000;
 
+localparam bltz = 5'b00000;
+localparam bgez = 5'b00001;
+localparam bltzal = 5'b10000;
+
 localparam mfc0 = 5'b00000;
 localparam mtc0 = 5'b00100;
 
@@ -117,7 +122,7 @@ localparam syscall = 6'b001100;
 
 localparam debug = 1;
 
-always @(*) begin
+always_comb begin
     regRead1Required = 0;
     regRead2Required = 0;
     if (currentStage == `stageD) begin
@@ -199,7 +204,7 @@ end
     regRead2 = rti;
 
 
-always @(*) begin
+always_comb begin
     memLoad = 0;
     memStore = 0;
     grfWriteSource = `grfWriteDisable;
@@ -273,6 +278,24 @@ always @(*) begin
                 end
             endcase
         end
+
+        REGIMM: begin
+            case (rti)
+                bltz: begin
+                    `simpleBranch
+                    cmpCtrl = `cmpLessThanZero;
+                end
+                bgez: begin
+                    `simpleBranch
+                    cmpCtrl = `cmpGreaterThanOrEqualToZero;
+                end
+
+                default: begin
+                    generateException = `ctrlUnknownInstruction;
+                end
+            endcase
+        end
+        
         R: begin
             case(funct)
                 addu: begin
@@ -522,10 +545,6 @@ always @(*) begin
             cmpCtrl = `cmpGreaterThanZero;
         end 
 
-        bltz_bgez: begin
-            `simpleBranch
-            cmpCtrl = rti == 1 ? `cmpGreaterThanOrEqualToZero : `cmpLessThanZero;
-        end
 
         lui: begin
             regRead1 = rsi;
