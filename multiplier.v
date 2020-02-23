ca7f5193f6f5f4742939b756ca5355e7db6711e5
Yunhao Tian <18373444@buaa.edu.cn>
2020-02-23 23:19:42 +0800

    Implement hardware multiplier and divider.

--- multiplier.v
+++ multiplier.v
@@ -12,24 +12,51 @@ module Multiplier (
            output reg [31:0] LO
        );
 
-localparam MultiplicationDelay = 5;
-localparam DivisionDelay = 10;
-
+localparam MultiplicationDelay = 7;
 reg [31:0] inA, inB;
-reg [3:0] counter;
 reg [3:0] op;
-reg [3:0] cycles;
 
-wire [63:0] multiplyResult = $signed(inA) * $signed(inB);
-wire [63:0] unsignedMultiplyResult = inA * inB;
+wire [63:0] multiplyResult;
+wire [63:0] unsignedMultiplyResult;
+mult_signed_0 signedMul(.A(inA), .B(inB), .CLK(clk), .SCLR(start), .P(multiplyResult));
+mult_unsigned_0 unsignedMul(.A(inA), .B(inB), .CLK(clk), .SCLR(start), .P(unsignedMultiplyResult));
 
+wire [63:0] signedDivResult;
+wire [63:0] unsignedDivResult;
+wire signedDivValid, unsignedDivValid;
+wire inputDataValid = busy;
+div_signed_0 signedDiv(
+    .aclk(clk),
+    .aresetn(!start),
+    .s_axis_divisor_tvalid(inputDataValid),
+    .s_axis_divisor_tdata(inB),
+    .s_axis_dividend_tvalid(inputDataValid),
+    .s_axis_dividend_tdata(inA),
+    .m_axis_dout_tdata(signedDivResult),
+    .m_axis_dout_tvalid(signedDivValid)
+);
+div_unsigned_0 unsignedDiv(
+    .aclk(clk),
+    .aresetn(!start),
+    .s_axis_divisor_tvalid(inputDataValid),
+    .s_axis_divisor_tdata(inB),
+    .s_axis_dividend_tvalid(inputDataValid),
+    .s_axis_dividend_tdata(inA),
+    .m_axis_dout_tdata(unsignedDivResult),
+    .m_axis_dout_tvalid(unsignedDivValid)
+);
+
+reg [3:0] counter;
+reg ready;
 always @(*) begin
-    cycles = 0;
+    ready = 0;
     case (op)
         `mtMultiply, `mtMultiplyUnsigned,`mtMSUB, `mtMADD, `mtMADDU:
-            cycles = MultiplicationDelay;
-        `mtDivide, `mtDivideUnsigned:
-            cycles = DivisionDelay;
+            ready = counter > MultiplicationDelay;
+        `mtDivide:
+            ready = signedDivValid;
+        `mtDivideUnsigned:
+            ready = unsignedDivValid;
     endcase
 end
 
@@ -38,6 +65,7 @@ always @(posedge clk) begin
         HI <= 0;
         LO <= 0;
         busy <= 0;
+        counter <= 'bX;
     end
     else if (start) begin
         if (ctrl == `mtSetHI) begin
@@ -50,19 +78,17 @@ always @(posedge clk) begin
             inA <= A;
             inB <= B;
             op <= ctrl;
-            counter <= 1;
+            counter <= 0;
             busy <= 1;
         end
     end
     else if (busy) begin
-        if (counter < cycles) begin
-            counter <= counter + 1;
-        end
-        else begin
+        if (ready) begin
+            counter <= 'bX;
             busy <= 0;
             case (op)
                 `mtMultiply:
-                    {HI, LO} <= $signed(inA) * $signed(inB);
+                    {HI, LO} <= multiplyResult;
                 `mtMSUB:
                     {HI, LO} <= {HI, LO} - multiplyResult;
                 `mtMADD:
@@ -70,21 +96,20 @@ always @(posedge clk) begin
                 `mtMADDU:
                     {HI, LO} <= {HI, LO} + unsignedMultiplyResult;
                 `mtMultiplyUnsigned:
-                    {HI, LO} <= inA * inB;
+                    {HI, LO} <= unsignedMultiplyResult;
                 `mtDivide: begin
                     if (inB != 0) begin
-                        HI <= $signed(inA) % $signed(inB);
-                        LO <= $signed(inA) / $signed(inB);
+                        {LO, HI} <= signedDivResult;
                     end
                 end
                 `mtDivideUnsigned: begin
                     if (inB != 0) begin
-                        HI <= inA % inB;
-                        LO <= inA / inB;
+                        {LO, HI} <= unsignedDivResult;
                     end
                 end
             endcase
-
+        end else begin
+            counter <= counter + 1;
         end
     end
 end
